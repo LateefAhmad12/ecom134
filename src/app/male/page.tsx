3b1defaa71497e6aa76fc47dc5f917e2ad226d26
Lateef <lateefahmad219@gmail.com>
2023-08-08 16:55:25 +0500

    key props added to male female and kids

--- src/app/male/page.tsx
+++ src/app/male/page.tsx
@@ -32,9 +32,9 @@ export default async function MaleProducts() {
         <section className=" mt-8">
             <div className=" flex flex-wrap gap-6">
             {data.map((item)=>(
-                      <Link 
+                      <Link key={item._id}
                       href={`/products/${item.slug.current}`}>
-                      <div key={item._id} className="mt-16">
+                      <div className="mt-16">
                       <Image src={urlForImage(item.image[0]).url()} alt={item.name} width={400} height={400} className="w-80 h-80"/>
                       <p className=" text-lg font-bold text-black">{item.name}</p>
                       <p className=" text-xl font-bold text-black">${item.price}</p>
